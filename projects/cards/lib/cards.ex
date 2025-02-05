362e990c6122908e66ab6639a8d7908738896d8c
HeikoKramer <github@myforce.net>
2025-02-05 19:58:19 +0100

    doctest added

--- projects/cards/lib/cards.ex
+++ projects/cards/lib/cards.ex
@@ -19,6 +19,21 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determins whether a deck contains a given card
+
+  ## Examples
+      iex(1)> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts",
+      "Ace of Diamons", "Two of Diamons", "Three of Diamons",
+      "Four of Diamons", "Five of Diamons"]
+      iex(2)> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -28,7 +43,7 @@ defmodule Cards do
     The `hand_size` argument indicates how many cards should
     be in the hand.
 
-    ## Examples
+  ## Examples
 
         iex> deck = Cards.create_deck
         iex> {hand, deck} = Cards.deal(deck, 1)
