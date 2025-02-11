86a153fd6d438f61fbe9f8234e7a257b949b42ae
HeikoKramer <github@myforce.net>
2025-02-11 15:43:41 +0100

    tutorial progress saved

--- projects/identicon/lib/identicon.ex
+++ projects/identicon/lib/identicon.ex
@@ -4,11 +4,20 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid = hex
-    |> Enum.chunk(3)
+    |> Enum.chunk_every(3, 3, :discard)
     |> Enum.map(&mirror_row/1)
     |> List.flatten
     |> Enum.with_index
@@ -27,7 +36,7 @@ defmodule Identicon do
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
